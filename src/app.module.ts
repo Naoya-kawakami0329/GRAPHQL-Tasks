6eabf0904d378cb38b654c2e3f072b825de1050b
Naoya-kawakami0329 <naoya.kawakami@arteryex.biz>
2025-09-23 02:05:19 +0900

    バックエンドセットアップ

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { TaskModule } from './task/task.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver:ApolloDriver,
+      playground:true,
+      autoSchemaFile:join(process.cwd(),'src/shema.gql'),
+      
+    }),
+    TaskModule
+  ],
+})
+export class AppModule {}
