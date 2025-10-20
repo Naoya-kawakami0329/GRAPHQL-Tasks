dde5231b05a5092ee83ee57b0e21ddc0ff3d98fc
Naoya-kawakami0329 <naoya.kawakami@arteryex.biz>
2025-10-20 22:50:43 +0900

    認証とルーティング

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
+import Main from './components/Main'
+import NotFound from './components/NotFound'
+import SignIn from './components/SIgnIn'
+import SignUp from './components/SignUp'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
 
   return (
-  <div className="App">
-    <h1>hello world</h1>
-  </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/singin' element={<SignIn/>}/>
+      <Route path='/signup' element={ <SignUp/>}/>
+      <Route path='/' element={ <Main/>}/>
+      <Route path='*' element={<NotFound/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/types/payload.ts
@@ -0,0 +1,6 @@
+export type payload={
+    email:string,
+    sub:number,
+    iat:number,
+    exp:number
+}
