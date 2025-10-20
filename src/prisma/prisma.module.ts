3335e9c5843b3963e987096024cba8b36a7870f6
Naoya-kawakami0329 <naoya.kawakami@arteryex.biz>
2025-10-20 21:51:36 +0900

    chore: clean project tree

--- src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Module({
-  imports:[PrismaModule],
-  providers: [PrismaService],
-  exports:[PrismaService],
-})
-export class PrismaModule {}
--- src/user/dto/getUser.args.ts
+++
@@ -1,9 +0,0 @@
-import { ArgsType, Field } from "@nestjs/graphql";
-import { IsEmail} from "class-validator";
-
-@ArgsType()
-export class GetUserArgs{
-    @Field()
-    @IsEmail()
-    email:string
-}
